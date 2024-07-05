ebfd8d112a197e84b816ef0cf5724e214be2ff17
lucianofc138 <lucianofc138@gmail.com>
2024-07-05 02:32:26 -0400

    feat(experience): dynamic table with preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lucianofc138.github.io',
     base: '/astro-portfolio',
+    integrations: [preact()],
 })
